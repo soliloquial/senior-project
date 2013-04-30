a65896753fcf4f3756debb41b102c744587e28d6
Brian <britchken@gmail.com>
2013-04-30 13:22:06 -0400

    Hey

--- code.py
+++ code.py
@@ -1,10 +1,32 @@
-instruments = [line.strip() for line in open('highdrums.txt')]
-rhythms = [line.strip() for line in open('highrhythms.txt')]
+from string import Template
+
+with open("Template.ly") as myfile:
+data ="".join(line.rstrip() for line in myfile)
+
+
+
+
+highinstruments = [line.strip() for line in open('highdrums.txt')]
+highrhythms = [line.strip() for line in open('highrhythms.txt')]
+lowinstruments = [line.strip() for line in open('lowdrums.txt')]
+lowrhythms = [line.strip() for line in open('low rhythms.txt')]
+
+
 from random import choice
-instrument = choice(instruments)
+hi_instrument = choice(highinstruments)
+lo_instrument = choice(lowinstruments)
 string = ""
 for i in xrange(4):
-	rhythm = choice(rhythms)
-	string = string + rhythm.replace('*', instrument) + " "
+	highrhythm = choice(highrhythms)
+	string = string + highrhythm.replace('*', hi_instrument) + " "
 highdrums = ((string+"\n")*10).strip()
-print highdrums
+string = ""
+for i in xrange(4):
+	lowrhythm = choice(lowrhythms)
+	string = string + lowrhythm.replace('*', lo_instrument) + " "
+lowdrums = ((string + "\n")*10).strip()
+
+song = Template(data)
+song.substitute(HighDrums=highdrums, LowDrums = 'stuff')
+
+print song
